f5ea58550a751849e888923f590b05af398b72ec
Don Welch <dwelch@nait.ca>
2025-09-16 21:31:23 -0600

    create XUnit Test project, setup package and project, demonstration TDD process of creating unit test then class code

---
+++ OPPsSolution/OPPsReview/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPPsReview
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ResidentAddress Address { get; set; }
+        public List<Employment> EmploymentPositions { get; set; }
+
+        public Person()
+        {
+            FirstName = "Unknown";
+            LastName = "Unknown";
+            //the default of a class is null
+            //Lits<T> is a class
+            //If I want a count of items in a class, one NEEDS an instance of the class
+            EmploymentPositions = new List<Employment>();
+        }
+
+        public Person(string firstname, string lastname, ResidentAddress address, 
+                        List<Employment> employmentpositions)
+        {
+            FirstName = firstname.Trim();
+            LastName = lastname.Trim();
+            Address = address;
+            if (employmentpositions == null)
+            {
+                EmploymentPositions = new List<Employment>();
+            }
+            else
+            {
+                EmploymentPositions = employmentpositions;
+            }
+        }
+    }
+}
---
+++ OPPsSolution/TDDxUnitTesting/Person_Should.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using OPPsReview;
+using FluentAssertions;
+#endregion
+
+namespace TDDxUnitTesting
+{
+    public class Person_Should
+    {
+        #region Constructors
+        #region Valid Data
+        //a Fact unit test executes once
+        //without the [Fact] annotation, the method is NOT considered a unit test
+        //  it would just be a method within this class
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_Default_Constructor()
+        {
+            //Arrange (this is the setup of values need for doing the test)
+            string expectedFirstName = "Unknown";
+            string expectedLastName = "Unknown";
+            int expectedEmploymentPositionsCount = 0;
+
+            //Act (this is the action that is under testing)
+            //sut: subject under test
+            //Image that the Act is a line of code from a program
+            Person sut = new Person();
+
+            //Assert (check the results of the act (Act) against expected Values (Arrange))
+            //this is where the FluentAssertions extension methods come into play
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().BeNull();
+            sut.EmploymentPositions.Count().Should().Be(expectedEmploymentPositionsCount);
+        }
+
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_Greedy_Constructor_With_No_Address_Or_Employments()
+        {
+            //Arrange (this is the setup of values need for doing the test)
+            string expectedFirstName = "Don";
+            string expectedLastName = "Welch";
+            int expectedEmploymentPositionsCount = 0;
+
+            //Act (this is the action that is under testing)
+            Person sut = new Person("  Don  ", "  Welch  ", null, null);
+
+            //Assert (check the results of the act (Act) against expected Values (Arrange))
+ 
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().BeNull();
+            sut.EmploymentPositions.Count().Should().Be(expectedEmploymentPositionsCount);
+        }
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_Greedy_Constructor_With_No_Employments()
+        {
+            //Arrange (this is the setup of values need for doing the test)
+            string expectedFirstName = "Don";
+            string expectedLastName = "Welch";
+            ResidentAddress expectedAddress = new ResidentAddress(123, "Maple St.",
+                            "Edmonton", "AB", "T6Y7U8");
+            int expectedEmploymentPositionsCount = 0;
+
+            //Act (this is the action that is under testing)
+            Person sut = new Person("  Don  ", "  Welch  ", expectedAddress, null);
+
+            //Assert (check the results of the act (Act) against expected Values (Arrange))
+
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().Be(expectedAddress);
+            sut.EmploymentPositions.Count().Should().Be(expectedEmploymentPositionsCount);
+        }
+
+        [Fact]
+        public void Successfully_Create_An_Instance_Using_Greedy_Constructor_With_Full_Data()
+        {
+            //Arrange (this is the setup of values need for doing the test)
+            string expectedFirstName = "Don";
+            string expectedLastName = "Welch";
+            ResidentAddress expectedAddress = new ResidentAddress(123, "Maple St.",
+                            "Edmonton", "AB", "T6Y7U8");
+
+            //how to test a collection?
+            //create individual instances of the item in the list
+            //in this example those instances are objects
+            //you must remember each object has a unique GUID
+            //NOTE: you CANNOT reuse a single variable to hold the separate instances
+            Employment one = new Employment("PG I", SupervisoryLevel.TeamMember,
+                                DateTime.Parse("2013/10/04"), 6.5);
+
+            //remember that if no year was supplied the length of holding the current
+            //      position is calculated for the instance
+            Employment two = new Employment("PG II", SupervisoryLevel.TeamLeader,
+                                DateTime.Parse("2020/04/04"));
+            List<Employment> employments = new List<Employment>();
+            employments.Add(one);
+            employments.Add(two);
+            int expectedEmploymentPositionsCount = 2;
+
+            //Act (this is the action that is under testing)
+            Person sut = new Person("  Don  ", "  Welch  ", expectedAddress, employments);
+
+            //Assert (check the results of the act (Act) against expected Values (Arrange))
+
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().Be(expectedAddress);
+            sut.EmploymentPositions.Count().Should().Be(expectedEmploymentPositionsCount);
+            //in addition to checking the number of items in your collection
+            //  you SHOULD check that the expected order of the items in your collection
+            //  is the same as what was sent in
+            sut.EmploymentPositions.Should().ContainInConsecutiveOrder(employments);
+        }
+        #endregion
+        #region Exception Testing
+        #endregion
+        #endregion
+
+        #region Properties
+        #region Valid Data
+        #endregion
+        #region Exception Testing
+        #endregion
+        #endregion
+
+        #region Methods
+        #region Valid Data
+        #endregion
+        #region Exception Testing
+        #endregion
+        #endregion
+    }
+}
